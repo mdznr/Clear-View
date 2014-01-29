83e1b15de346318e9ffd4d8bac76a96ed8632efd
Matt Zanchelli <matt@mdznr.com>
2014-01-29 18:21:23 -0500

    Initial Commit

---
+++ Camera/MTZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTZAppDelegate.h
+//  Camera
+//
+//  Created by Matt Zanchelli on 1/29/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraTests/CameraTests.m
@@ -0,0 +1,34 @@
+//
+//  CameraTests.m
+//  CameraTests
+//
+//  Created by Matt Zanchelli on 1/29/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CameraTests : XCTestCase
+
+@end
+
+@implementation CameraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
